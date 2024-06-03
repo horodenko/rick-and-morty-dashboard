ad3ca2867012b5b38bb5f318f448eec9365470ed
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2024-06-03 11:26:19 -0300

    refactor: new dashboard

---
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent<T> {
+  @Input() data: T[] = [];
+}
