fae3bbc37e63033c95d8b33f991dccdcd8e75ea9
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2024-06-04 11:13:19 -0300

    chore: header changes

--- src/app/core/components/header/pages/header/header.component.ts
+++ src/app/core/components/header/pages/header/header.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { SidebarComponent } from '../../../sidebar/pages/sidebar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+
+    SidebarComponent,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  @ViewChild('drawer') drawer!: MatDrawer;
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.onSetDrawerMode();
+  }
 
+  ngAfterViewInit(): void {
+    this.onSetDrawerMode();
+  }
+
+  onSetDrawerMode = () =>
+    window.innerWidth < 768
+      ? (this.drawer.mode = 'over')
+      : (this.drawer.mode = 'side');
 }
