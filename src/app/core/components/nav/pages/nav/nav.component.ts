9948dfe12d844a31e4f9052c90b1e5dca3dc5abf
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2024-06-06 13:04:07 -0300

    feat: change header style to bootstrap / get username to display

--- src/app/core/components/nav/pages/nav/nav.component.ts
+++ src/app/core/components/nav/pages/nav/nav.component.ts
@@ -1,23 +1,19 @@
-import {
-  AfterViewChecked,
-  Component,
-  EventEmitter,
-  HostListener,
-  Output,
-  ViewChild,
-} from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { NavigationStart, Router, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { UserService } from '../../../../../features/login/services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
     RouterModule,
+    CommonModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
@@ -28,6 +24,18 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  constructor(private userService: UserService, private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        if (event.url === '/sign-in') {
+          this.drawer.close();
+        }
+      }
+    });
+  }
+
+  username: string = '';
+
   @ViewChild('drawer') drawer!: MatDrawer;
   @HostListener('window:resize')
   onWindowResize() {
@@ -36,12 +44,23 @@ export class NavComponent {
 
   protected isOpen: boolean = false;
 
+  ngOnInit(): void {
+    this.userService.onChangeUsername(this.userService.onGetStorageKey());
+    this.userService.currentUsername$.subscribe(
+      username => (this.username = username)
+    );
+  }
+
   ngAfterViewChecked(): void {
     this.onSetDrawerMode();
   }
 
-  onSetDrawerMode = () =>
+  onSetDrawerMode = (): string =>
     window.innerWidth < 768
       ? (this.drawer.mode = 'over')
       : (this.drawer.mode = 'side');
+
+  isUserAuthenticated(): boolean {
+    return this.userService.isAuthenticated();
+  }
 }
