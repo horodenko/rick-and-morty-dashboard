69fb20e7be89480207155b67a9a0314bcac4ee0a
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2024-06-06 13:05:28 -0300

    feat: add login page

--- src/app/features/login/pages/login.component.ts
+++ src/app/features/login/pages/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  protected username: string = '';
+
+  onLogin(): void {
+    this.userService.onChangeUsername(this.username);
+    this.router.navigate(['/characters']);
+  }
 }
