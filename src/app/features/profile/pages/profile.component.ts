4588d7be3e3210166f901a87f27b67ccea45ac65
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2024-06-06 13:05:48 -0300

    feat: add profile page

--- src/app/features/profile/pages/profile.component.ts
+++ src/app/features/profile/pages/profile.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { UserService } from '../../login/services/user.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  constructor(private userService: UserService) {}
 
+  protected username: string = '';
+
+  ngOnInit(): void {
+    this.userService.currentUsername$.subscribe(
+      username => (this.username = `${username}, the cool one`)
+    );
+  }
 }
