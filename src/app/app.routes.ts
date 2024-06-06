53027ace9f1806b5394b8feec59560f23ff22e15
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2024-06-06 13:01:11 -0300

    feat: new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,22 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/pages/home.component';
 import { LocationsComponent } from './features/locations/pages/locations/locations.component';
 import { CharactersComponent } from './features/characters/pages/characters/characters.component';
 import { EpisodesComponent } from './features/episodes/pages/episodes/episodes.component';
 import { EpisodeDetailComponent } from './features/episodes/pages/episodes/episode-detail/episode-detail.component';
 import { LocationDetailComponent } from './features/locations/pages/locations/location-detail/location-detail.component';
 import { CharacterDetailComponent } from './features/characters/pages/characters/character-detail/character-detail.component';
+import { LoginComponent } from './features/login/pages/login.component';
+import { ProfileComponent } from './features/profile/pages/profile.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: LoginComponent },
+  { path: 'user-profile', component: ProfileComponent },
   { path: 'characters', component: CharactersComponent },
   { path: 'characters/:id', component: CharacterDetailComponent },
   { path: 'locations', component: LocationsComponent },
   { path: 'locations/:id', component: LocationDetailComponent },
   { path: 'episodes', component: EpisodesComponent },
+  { path: 'episodes/:id', component: EpisodeDetailComponent },
   { path: 'episodes/:id', component: EpisodeDetailComponent },
 ];
