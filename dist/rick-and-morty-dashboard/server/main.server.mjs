5f1299088b16a70cb0c180be79a74a86997ce895
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2024-06-07 23:45:39 -0300

    chores

--- dist/rick-and-morty-dashboard/server/main.server.mjs
+++ dist/rick-and-morty-dashboard/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NFW3V4CZ.mjs";import"./chunk-LJOYFTYN.mjs";import"./chunk-2M5LCP4K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-K6MJJMFX.mjs";import"./chunk-QCGLCOZT.mjs";import"./chunk-2M5LCP4K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
